cdd7ce96c12be9893d0b8e597d27b8255b7d7c44
Marino <107618043+marin-o@users.noreply.github.com>
2024-07-05 16:20:32 +0200

    Update MONGO_URI in config.py to use MongoDB replica set

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 class Config:
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/music_publisher')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://rootuser:rootpassword@mongo-0.mongo-headless.kiii.svc.cluster.local:27017,mongo-1.mongo-headless.kiii.svc.cluster.local:27017,mongo-2.mongo-headless.kiii.svc.cluster.local:27017/music_publisher?replicaSet=rs0')
